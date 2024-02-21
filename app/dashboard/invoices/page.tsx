bb5627bc2beebc81c2accb2465327eff5a60ddcb
rafifrizqullah <rafifrizqullah@users.noreply.github.com>
2024-02-21 21:31:55 +0700

    Update invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -10,7 +10,10 @@ import { fetchInvoicesPages } from '@/app/lib/data';
 export default async function Page({
   searchParams,
 }: {
-  searchParams?: { query?: string; page?: string };
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
